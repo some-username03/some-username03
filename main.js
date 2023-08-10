a5ceb42fa53a4837fe6da89c8e47181f717fb65e
Yunrui <50620535+some-username03@users.noreply.github.com>
2023-08-10 21:06:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const ig = document.querySelector(".ig");
+const linkedIn = document.querySelector(".linkedIn");
 const yt = document.querySelector(".yt");
 const fb = document.querySelector(".fb");
 const contact = document.querySelector(".contact");
 
 ig.onclick = () => {
     window.open("https://www.instagram.com/wyr03");
+};
+
+linkedIn.onclick = () => {
+    window.open("https://www.linkedin.com/in/yunrui-wang-5903a71a3/");
 };
 
 yt.onclick = () => {
