676af796c7b8edb37f65a1db3ae6592116d9c3aa
Yunrui <50620535+some-username03@users.noreply.github.com>
2023-08-10 12:35:56 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const ig = document.querySelector(".ig");
+const yt = document.querySelector(".yt");
+const fb = document.querySelector(".fb");
+const contact = document.querySelector(".contact");
+
+ig.onclick = () => {
+    window.open("https://www.instagram.com/wyr03");
+};
+
+yt.onclick = () => {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley");
+};
+
+fb.onclick = () => {
+    window.open("https://www.facebook.com/");
+};
+
+contact.onclick = () => {
+    window.open("mailto:yunruiw@gmail.com");
+};
